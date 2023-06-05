0b5e96b0082fb82607d9eac1c149c7adf8c86834
brennan lamey <brennanlamey@gmail.com>
2023-06-05 17:46:19 -0500

    added custom error func to replace RAISE

---
+++ ext/error/error.go
@@ -0,0 +1,24 @@
+package error
+
+import (
+	"fmt"
+
+	"github.com/kwilteam/go-sqlite"
+)
+
+// impl is the implementation of the ERROR function.
+var impl = &sqlite.FunctionImpl{
+	NArgs:         1,
+	Deterministic: true,
+	AllowIndirect: true,
+	Scalar:        errorFunc,
+}
+
+// Register registers the "ERROR" function on the given connection.
+func Register(c *sqlite.Conn) error {
+	return c.CreateFunction("regexp", impl)
+}
+
+func errorFunc(ctx sqlite.Context, args []sqlite.Value) (sqlite.Value, error) {
+	return sqlite.Value{}, fmt.Errorf("error: %s", args[0].Text())
+}
