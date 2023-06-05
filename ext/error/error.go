110f097f5ca7a59dd0665051675f5d127fc2d397
brennan lamey <brennanlamey@gmail.com>
2023-06-05 17:55:39 -0500

    fixed naming issue on ERROR function

--- ext/error/error.go
+++ ext/error/error.go
@@ -16,7 +16,7 @@ var impl = &sqlite.FunctionImpl{
 
 // Register registers the "ERROR" function on the given connection.
 func Register(c *sqlite.Conn) error {
-	return c.CreateFunction("regexp", impl)
+	return c.CreateFunction("error", impl)
 }
 
 func errorFunc(ctx sqlite.Context, args []sqlite.Value) (sqlite.Value, error) {
